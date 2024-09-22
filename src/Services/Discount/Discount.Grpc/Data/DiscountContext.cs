54ec37ac2cff447e8374078025536c242426bf7e
NehemiahLimo <nehemiahlimo02@gmail.com>
2024-09-22 03:50:19 +0300

    Added DbContext SQLite dependency

--- src/Services/Discount/Discount.Grpc/Data/DiscountContext.cs
+++ src/Services/Discount/Discount.Grpc/Data/DiscountContext.cs
@@ -10,6 +10,14 @@ namespace Discount.Grpc.Data
         public DiscountContext(DbContextOptions<DiscountContext> options) : base(options) 
         {
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Coupon>().HasData(
+                new Coupon { Id = 1, ProductName = "Mandazi", ProductDescription = "Ni ya mahamri buana", Amount = 20 },
+                new Coupon { Id = 2, ProductName = "Chapati", ProductDescription = "Chapo chapo", Amount = 40 }
+                );
+        }
     }
 
 }
---
+++ src/Services/Discount/Discount.Grpc/Migrations/20240922002856_InitialMigration.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Discount.Grpc.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Coupons",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ProductName = table.Column<string>(type: "TEXT", nullable: true),
+                    ProductDescription = table.Column<string>(type: "TEXT", nullable: true),
+                    Amount = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Coupons", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Coupons",
+                columns: new[] { "Id", "Amount", "ProductDescription", "ProductName" },
+                values: new object[,]
+                {
+                    { 1, 20, "Ni ya mahamri buana", "Mandazi" },
+                    { 2, 40, "Chapo chapo", "Chapati" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Coupons");
+        }
+    }
+}
